f81a21a54ccf6053fc59f5df737ec70b494fd262
jrazap <elazapm@gmail.com>
2024-08-29 17:39:56 +0200

    Dynamic Parallel Queries | View Data

--- src/components/DynamicParallel.page.jsx
+++ src/components/DynamicParallel.page.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
 import { useQueries } from "react-query";
+import { Link } from "react-router-dom";
 
 const fetchSuperHero = (heroId) => {
   return axios.get(`${import.meta.env.VITE_APP_API_URL}/superheroes/${heroId}`);
@@ -22,6 +23,25 @@ const DynamicParallelPage = ({ heroIds }) => {
     <section className="my-4">
       <div className="container">
         <h1 className="display-4 mb-4">Dynamic Parallel Page</h1>
+        <div className="row">
+          {queryResults?.map((item, idx) => (
+            <div className="col-lg-4" key={`hero-${idx}`}>
+              <Link
+                to={`/rq-super-heroes/${item.data?.data.id}`}
+                className="card mb-3"
+              >
+                <div className="row g-0">
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">{item.data?.data.name}</h5>
+                      <p className="card-text">{item.data?.data.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
